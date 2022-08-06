d1fad982e6c794244cd4f6dd68c9dd74fe7c8c66
Andr0medAonair <sagethirteen@gmail.com>
2022-08-06 18:03:49 -0300

    fix: removed useless commas

--- controls.js
+++ controls.js
@@ -1,9 +1,9 @@
 class Controls {
     constructor(){
-        this.forward = false,
-        this.left = false,
-        this.right = false,
-        this.reverse = false,
+        this.forward = false
+        this.left = false
+        this.right = false
+        this.reverse = false
         this.#addKeyboardListeners()
     }
 
