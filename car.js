fd7e08e832ae3be8018138d7745e155e06a73ece
Andr0medAonair <sagethirteen@gmail.com>
2022-08-06 17:11:32 -0300

    impl: car driving mechanics completed

--- car.js
+++ car.js
@@ -8,10 +8,15 @@ class Car {
         this.acceleration = .2
         this.maxSpeed = 3
         this.friction = .05
+        this.angle = 0
         this.controls = new Controls()
     }
 
     update(){
+        this.#move()
+    }
+    
+    #move(){
         if(this.controls.forward)
             this.speed += this.acceleration
         if(this.controls.reverse)
@@ -26,21 +31,29 @@ class Car {
             this.speed += this.friction
         if(Math.abs(this.speed) < this.friction)
             this.speed = 0
-        if(this.controls.left)
-            this.x -= 2
-        if(this.controls.right)
-            this.x += 2
-        this.y -= this.speed
+        if(this.speed!=0) {
+            const flip = this.speed > 0 ? 1 : -1
+            if(this.controls.left)
+                this.angle += .03 * flip
+            if(this.controls.right)
+                this.angle -= .03 * flip
+    }
+    this.x -= Math.sin(this.angle) * this.speed
+    this.y -= Math.cos(this.angle) * this.speed
     }
 
     draw(ctx) {
+        ctx.save()
+        ctx.translate(this.x, this.y)
+        ctx.rotate(-this.angle)
         ctx.beginPath()
         ctx.rect(
-            this.x - this.width/2,
-            this.y - this.height/2,
+            - this.width/2,
+            - this.height/2,
             this.width,
             this.height
         )
         ctx.fill()
+        ctx.restore()
     } 
 }
